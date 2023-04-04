d808947ce116c15620af8b71b2f5fdba744a2be9
Ekaterina <ekhomiakov@yandex.ru>
2023-04-04 14:24:42 +0300

    Задача 43

--- Задача 43/Program.cs
+++ Задача 43/Program.cs
@@ -1,21 +1,21 @@
-﻿// Напишите программу, которая найдет точку пересечения двух прямых ,заданных уравнениями 
-// y = k1*x+b1, y=k2*x+b2; значения b1, k1, b2 задаются пользователем
-// b1 = 2, k1=5, b2=4, k2 =9 -> (-0,5; -0,5)
-const int COEFFICIENT = 0;
-const int CONSTANT =1;
-const int X_COORD =0;
-const int Y_COORD =1;
-const int LINE1 =1;
-const int LINE2 =2;
+﻿// Напишите программу, которая найдет точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2*x +b2;
+// значения b1, k1, b2 и k2 задаются пользователем
+// b1=2, k1 =5, b2= 4, k2 =9 -> (-0,5;-0,5)
+Console.Clear();
+Console.Write("Введите значения b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
 
-double[] lineData1 = InputLineData(LINE1);
-double[] lineData2 = InputLineData(LINE2);
+double x = -(b1-b2)/ (k1-k2);
+double y = k2*x +b2;
+x=Math.Round(x,3);
+y=Math.Round(y,3);
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
 
-if (ValidateLines(lineData1, lineData2))
-{
-    double[] coord = FindCoords (lineData1, lineData2);
-    Console.Write($"Точка пересечения уравнений  y="{lineData1[COEFFICIENT]}*x+{lineData1[CONSTANT]} и 
-    y= {lineData2[COEFFICIENT]}*x+{lineData2[CONSTANT]}");
-    Console.WriteLine($"имеет координаты ({coord[X_COORD]},{coord[Y_COORD]})")
-} 
-//Ввод числа
+
